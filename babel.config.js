090b513de07be6c9f424dda7221deea44df21f20
Usu <usuedeaghe@gmail.com>
2024-08-14 21:33:57 +0100

    test setup

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,6 @@
-module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-react"],
-  plugins: ["@babel/plugin-transform-runtime"],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+  };
 };
---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: "react-native",
+  setupFilesAfterEnv: ["<rootDir>/jest-setup.js"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+  },
+  transformIgnorePatterns: [
+    "node_modules/(?!(@react-native|react-native|react-navigation|@react-navigation))",
+  ],
+};
