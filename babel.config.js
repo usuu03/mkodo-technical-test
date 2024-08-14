9a1c83e0ece61f953317db8ae340440b2a6318b7
Usu <usuedeaghe@gmail.com>
2024-08-14 21:35:31 +0100

    ESSENTIAL TASK COMPLETED

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,13 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ["babel-preset-expo"],
-  };
+module.exports = {
+  presets: [
+    "module:metro-react-native-babel-preset",
+    "@babel/preset-env",
+    "@babel/preset-react",
+    "@babel/preset-typescript",
+  ],
+  plugins: [
+    "@babel/plugin-transform-runtime",
+    "@babel/plugin-proposal-class-properties",
+    "@babel/plugin-proposal-object-rest-spread",
+  ],
 };
