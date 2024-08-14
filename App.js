8d624cc200673b3df067e3392b8a0c15ba0e2b08
Usu <usuedeaghe@gmail.com>
2024-08-14 23:28:50 +0100

    fatal error fix

--- App.js
+++ App.js
@@ -1,26 +1,5 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import LotteryDrawDetail from "./components/LotteryDrawDetail";
 import LotteryDrawList from "./components/LotteryDrawList";
 
-const Stack = createStackNavigator();
-
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="LotteryDrawList">
-        <Stack.Screen
-          name="LotteryDrawList"
-          options={{ title: "Lottery Draws" }}
-        >
-          {(props) => <LotteryDrawList {...props} draws={draws} />}
-        </Stack.Screen>
-        <Stack.Screen
-          name="LotteryDrawDetail"
-          component={LotteryDrawDetail}
-          options={{ title: "Draw Details" }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <LotteryDrawList />;
 }
--- components/LotteryDrawList.jsx
+++ components/LotteryDrawList.jsx
@@ -1,33 +1,19 @@
-import { useNavigation } from "@react-navigation/native";
-import { render } from "@testing-library/react-native";
 import React from "react";
-import {
-  FlatList,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-} from "react-native";
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import lotteryData from "../utils/lotteryData";
 
 export default function LotteryDrawList() {
-  const navigation = useNavigation();
-
-  const renderItem = ({ item }) => (
-    <TouchableOpacity
-      style={styles.item}
-      onPress={() => navigation.navigate("LotteryDrawDetail", { draw: item })}
-    >
-      <Text style={styles.drawDate}>{item.date}</Text>
-    </TouchableOpacity>
-  );
-
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>List of Draws</Text>
       <FlatList
         data={lotteryData.draws}
-        renderItem={render}
+        renderItem={({ item }) => (
+          <View style={styles.item}>
+            <Text style>{item.id}</Text>
+            <Text>{item.drawDate}</Text>
+          </View>
+        )}
         keyExtractor={(item) => item.id}
       />
     </SafeAreaView>
@@ -43,18 +29,15 @@ const styles = StyleSheet.create({
   title: {
     marginTop: 15,
     fontSize: 35,
-    marginRight: 50,
-    marginLeft: 110,
     fontWeight: "bold",
-  },
-  drawDate: {
-    fontSize: 18,
+    marginRight: 100,
+    marginLeft: 100,
   },
   item: {
     backgroundColor: "#80ccff",
     padding: 15,
     fontSize: 15,
-    marginTop: 5,
+    marginTop: 10,
     marginRight: 15,
     marginLeft: 15,
     borderRadius: 10,
