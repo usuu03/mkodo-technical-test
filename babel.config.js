54950e7f40709e07aadf9e18fd68565caa492b89
Usu <usuedeaghe@gmail.com>
2024-08-14 21:59:00 +0100

    testing works

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,6 @@
-module.exports = {
-  presets: [
-    "module:metro-react-native-babel-preset",
-    "@babel/preset-env",
-    "@babel/preset-react",
-    "@babel/preset-typescript",
-  ],
-  plugins: [
-    "@babel/plugin-transform-runtime",
-    "@babel/plugin-proposal-class-properties",
-    "@babel/plugin-proposal-object-rest-spread",
-  ],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+  };
 };
