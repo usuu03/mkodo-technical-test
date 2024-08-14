1c6e2b563d7c2c5c71f674949542a280ea2dd92f
Usu <usuedeaghe@gmail.com>
2024-08-14 22:17:13 +0100

    setup for additional task

--- App.js
+++ App.js
@@ -1,5 +1,26 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import LotteryDrawDetail from "./components/LotteryDrawDetail";
 import LotteryDrawList from "./components/LotteryDrawList";
 
+const Stack = createStackNavigator();
+
 export default function App() {
-  return <LotteryDrawList />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="LotteryDrawList">
+        <Stack.Screen
+          name="LotteryDrawList"
+          options={{ title: "Lottery Draws" }}
+        >
+          {(props) => <LotteryDrawList {...props} draws={draws} />}
+        </Stack.Screen>
+        <Stack.Screen
+          name="LotteryDrawDetail"
+          component={LotteryDrawDetail}
+          options={{ title: "Draw Details" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
